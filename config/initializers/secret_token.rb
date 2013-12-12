1d6b7baea8aad26a56d918ee2f4f1dc0ec7e180b
Kirillov Alexander <saratovsource@gmail.com>
2013-12-12 14:12:48 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PowerLanding::Application.config.secret_token = 'a6900642ebcb34db93eec2a25576587beaff5e7b260abcb041d4222f655fe7bb9dbb0009658bb6e976e93fb0725c6ef7fdeaef34f12e7263cc336d1dcf23e8e4'

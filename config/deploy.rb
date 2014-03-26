a2b9bc06a17e640bb3dea2b651cfbb763faa1d26
Alexander Kirillov <saratovsource@gmail.com>
2014-03-26 23:10:04 +0400

    More changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ role :web,            deploy_server
 role :app,            deploy_server
 role :db,             deploy_server, :primary => true
 
-set :rvm_ruby_string, "2.0.0"
+set :rvm_ruby_string, "2.1.0"
 set :rake,            "rvm use #{rvm_ruby_string} do bundle exec rake" 
 set :bundle_cmd,      "rvm use #{rvm_ruby_string} do bundle"
 
